0b891e853d453d4c5618543b0f055384912b5130
N1GH7C4P <kimmo.polojarvi@gmail.com>
2022-04-17 20:52:32 +0300

    it is what it is

--- basket.php
+++ basket.php
@@ -8,7 +8,7 @@
 </head>
 <body style="margin-left: 5%; margin-top: 5%; margin-right: 5%; margin-bottom: 5%;">
 	<div class="form-style-3">
-	<span><?=$SESSION_['loggued_on_user']?>'s Basket</span>
+	<span><?=$SESSION['loggued_on_user']?>'s Basket</span>
 	</div>
 </body>
 </html>
