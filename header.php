5ef79fcaedcfe391e961893a2f49d57bc70681bd
Anton Kilk <akilk@student.hive.com>
2022-04-17 19:45:41 +0300

    anton frontend suff

---
+++ header.php
@@ -0,0 +1,33 @@
+<?php
+	session_start();
+?>
+<div class="menu">
+	<div class="menu-sub">
+		<a href="index.html">
+			<img src="img/logo.svg" alt="logo">
+		</a>
+		<a href="#">
+			<img src="img/search.svg" alt="search">
+		</a>
+	</div>
+	<div class="menu-sub">
+		<a href="#">
+			<img src="img/menu.svg" alt="menu">
+		</a>
+		<a href="#">
+			<img src="img/login.svg" alt="login">
+		</a>
+		<a href="#">
+			<img src="img/basket.svg" alt="basket">
+		</a>
+	</div>
+</div>
+<div class="header">
+	<div class="header-text">
+		<div><img src="img/palka.png" alt="stick"></div>
+		<div>
+			<h2>ANTON</h2>
+			<h2>& KIMMO <span id="red">FASHION</span></h2>
+		</div>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	//connect to database
+	$root = $_SERVER['DOCUMENT_ROOT']."/rush00";
 	$servername = "localhost";
 	$username = "root";
 	$password = "1233anton";
@@ -10,6 +10,7 @@
 	//check video settings if this doesn't work
 	$category = mysqli_query($connection, "SELECT * FROM category");
 	$products = mysqli_query($connection, "SELECT * FROM products");
+	$clothes= mysqli_query($connection, "SELECT * FROM clothes_type");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,17 +18,69 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Online shop</title>
+	<link rel="stylesheet" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<div>helllo World!</div>
-	<div>
-		<?php while ($product = mysqli_fetch_assoc($products)) : ?>
-			<h4><?=$product['title'] ?></h4>
-			<img src="<?=$product['image']; ?>" alt="alt">
-			<p><?=$product['description'] ?></p>
-		<?php endwhile; ?>
-	</div>
+	<?php include $root."/header.php"; ?>
+	<div class="container">
+		<div class="content">
+			<div class="top-container">
+				<div class="deal"><h2>
+					30% OFF
+				</h2>
+					<h1>
+					<a href="getcategory.php?category=women"><li>
+					<?php
+						$cat=mysqli_fetch_assoc($category);
+						print_r($cat['title']);
+						// if this clicked: show men clothes only
 
+					?>
+					</li></a>
+					</h1></div>
+				<div class="deal"><h2>
+					HOT DEAL
+				</h2>
+				<h1>
+					<a href="getcategory.php?category=men"><li>
+					<?php
+						$cat=mysqli_fetch_assoc($category);
+						print_r($cat['title']);
+						// if this clicked: show men clothes only
+
+					?>
+					</li></a>
+				</h1></div>
+				<div class="deal"><h2>NEW ARRIVALS</h2><h1>
+					<a href="getcategory.php?category=kids"><li>
+						<?php
+							$cat=mysqli_fetch_assoc($category);
+							print_r($cat['title']);
+							// if this clicked: show men clothes only
+
+						?>
+					</li></a>
+				</h1></div>
+			</div>
+			<div class="featured-items">
+				<h1>Products</h1>
+				<div class="products-list">
+					<?php while ($product = mysqli_fetch_assoc($products)) : ?>
+						<div class="products">
+							<h2><?=$product['title'] ?></h2>
+							<img class="" src="<?=$product['image']; ?>" alt="alt">
+							<p><?=$product['description'] ?></p>
+							<p><?=$product['price']?>$</p>
+						</div>
+						<?php endwhile; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<p>	&#9400; Anton & Kimmo corporation</p>
+	</div>
 </body>
 </html>
